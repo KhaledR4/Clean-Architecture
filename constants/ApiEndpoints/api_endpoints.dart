3a3753a453688ebee7425e6d5a43eaf8a0684045
Khaled Ramadan <94381448+KhaledR4@users.noreply.github.com>
2023-08-03 08:44:05 +0300

    edit api endpoints

--- constants/ApiEndpoints/api_endpoints.dart
+++ constants/ApiEndpoints/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints{
-  static const String base = "http://192.168.1.6:5000/api/";
+  static const String base = "http://localhost:5000/api/";
   static const String signUp = "${base}signup/post";
   static String checkTokenValidation(token) => "${base}sigin/token?token=$token";
   static String checkUserCredentials(email, password) => "${base}signin/login?email=$email&password=$password";
